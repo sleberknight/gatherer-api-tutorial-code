a82852448fa07830aebfb6680dbe3e7eb8a0ec00
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2025-04-22 20:25:20 +0000

    Add Example_09_RemoveDupsGatherer demonstrating distinct gatherer functionality

---
+++ src/main/java/com/acme/Example_09_RemoveDupsGatherer.java
@@ -0,0 +1,28 @@
+package com.acme;
+
+import java.util.HashSet;
+import java.util.function.Supplier;
+import java.util.stream.Gatherer;
+import java.util.stream.Stream;
+
+public class Example_09_RemoveDupsGatherer {
+
+    static <E> Gatherer<E, ?, E> distinct() {
+        Supplier<HashSet<E>> initializer = HashSet::new;
+        Gatherer.Integrator<HashSet<E>, E, E> integrator = (set, element, downstream) -> {
+            if (set.add(element)) {
+                downstream.push(element);
+            }
+            return true; // this is not the right thing to do even though it works here...see article
+        };
+        Gatherer<E, ?, E> gatherer = Gatherer.ofSequential(initializer, integrator);
+        return gatherer;
+    }
+
+    public static void main(String[] args) {
+        var result = Stream.of(1, 2, 3, 4, 5, 4, 3, 2, 5, 5, 1)
+                .gather(distinct())
+                .toList();
+        System.out.println("result = " + result);
+    }
+}
