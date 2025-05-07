de4f789fdfd1112571b311726629c46bc40b8662
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2025-05-07 21:45:36 +0000

    Add examples for limiting elements in gatherers
    Add Example_10_LimitingElements_N demonstrating limiting functionality in gatherers, where N is 1 and 2

---
+++ src/main/java/com/acme/Example_10_LimitingElements_1.java
@@ -0,0 +1,34 @@
+package com.acme;
+
+import java.util.function.Supplier;
+import java.util.stream.Gatherer;
+import java.util.stream.Stream;
+
+public class Example_10_LimitingElements_1 {
+
+    static <E> Gatherer<E, ?, E> limit(long limit) {
+        class Box {
+            long counter = 0L;
+        }
+        Supplier<Box> initializer = Box::new;
+        Gatherer.Integrator<Box, E, E> integrator = (box, element, downstream) -> {
+            if (downstream.isRejecting()) {  // don't actually need this...see next example
+                return false;
+            } else if (box.counter < limit) {
+                box.counter++;
+                return downstream.push(element);
+            } else {
+                return false;
+            }
+        };
+        Gatherer<E, ?, E> gatherer = Gatherer.ofSequential(initializer, integrator);
+        return gatherer;
+    }
+
+    public static void main(String[] args) {
+        var result = Stream.of(1, 2, 3, 4, 5)
+                .gather(limit(3))
+                .toList();
+        System.out.println("result = " + result);
+    }
+}
